0c9d6fdfa233cf024655d87a40deea60c0377e68
Steven Mercatante <steven.mercatante@gmail.com>
2019-07-13 14:56:34 -0400

    add test content

--- pages/demos/mario.js
+++ pages/demos/mario.js
@@ -1,8 +1,23 @@
 import Timeline from "@merc/react-timeline";
 
 const events = [
-  { type: "text", date: "7/13/19", text: "Hello!" },
-  { type: "text", date: "7/13/19", text: "What's for lunch?" }
+  {
+    type: "image",
+    date: "7/13/19",
+    src:
+      "https://upload.wikimedia.org/wikipedia/en/0/03/Super_Mario_Bros._box.png"
+  },
+  {
+    type: "image",
+    date: "7/13/19",
+    src: "https://upload.wikimedia.org/wikipedia/en/0/00/Super_Mario_Bros_2.jpg"
+  },
+  {
+    type: "image",
+    date: "7/13/19",
+    src:
+      "https://upload.wikimedia.org/wikipedia/en/a/a5/Super_Mario_Bros._3_coverart.png"
+  }
 ];
 
 export default function Mario() {
