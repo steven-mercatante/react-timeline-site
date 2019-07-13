799717fcd223c2033cc37e20c74fb67f109b2719
Steven Mercatante <steven.mercatante@gmail.com>
2019-07-13 13:04:05 -0400

    use react-timeline v0.1.13

--- pages/demos/mario.js
+++ pages/demos/mario.js
@@ -1,6 +1,9 @@
-import { Timeline } from "@merc/react-timeline";
+import Timeline from "@merc/react-timeline";
 
-const events = [{ type: "text", date: "7/13/19", text: "Hello!" }];
+const events = [
+  { type: "text", date: "7/13/19", text: "Hello!" },
+  { type: "text", date: "7/13/19", text: "What's for lunch?" }
+];
 
 export default function Mario() {
   return <Timeline events={events} />;
