afdd6bf0f68868d050b4fdd457c74694ff16dbc7
Steven Mercatante <steven.mercatante@gmail.com>
2019-07-13 21:35:36 -0400

    adding dummy mario data

--- pages/demos/mario.js
+++ pages/demos/mario.js
@@ -4,20 +4,20 @@ const events = [
   {
     type: "image",
     date: "7/13/19",
-    src:
-      "https://upload.wikimedia.org/wikipedia/en/0/03/Super_Mario_Bros._box.png"
+    src: "/static/images/mario/smb.png"
   },
   {
     type: "image",
     date: "7/13/19",
-    src: "https://upload.wikimedia.org/wikipedia/en/0/00/Super_Mario_Bros_2.jpg"
+    src: "/static/images/mario/smb2.jpg"
   },
   {
     type: "image",
     date: "7/13/19",
-    src:
-      "https://upload.wikimedia.org/wikipedia/en/a/a5/Super_Mario_Bros._3_coverart.png"
-  }
+    src: "/static/images/mario/smb3.png"
+  },
+  { type: "youtube", date: "7/13/19", id: "j8YFxB6rYIo" },
+  { type: "twitter", date: "7/13/19", id: "1149702455590105089" }
 ];
 
 export default function Mario() {
