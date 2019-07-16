48fde35961d8d68d45034ee7033870224d0f44c3
Steven Mercatante <steven.mercatante@gmail.com>
2019-07-16 16:56:48 -0400

    trying to SSR styled-components

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,15 @@
 import React from "react";
 import App, { Container } from "next/app";
 import * as Sentry from "@sentry/browser";
+import { ThemeProvider } from "styled-components";
 
 Sentry.init({
   dsn: "https://2c7b645bba274e7b9421275eee5d50f1@sentry.io/1505055"
 });
+
+const theme = {
+  colors: { primary: "#0070f3" }
+};
 
 export default class MyApp extends App {
   static async getInitialProps({ Component, ctx }) {
@@ -34,7 +39,9 @@ export default class MyApp extends App {
 
     return (
       <Container>
-        <Component {...pageProps} />
+        <ThemeProvider theme={theme}>
+          <Component {...pageProps} />
+        </ThemeProvider>
       </Container>
     );
   }
